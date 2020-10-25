44e0289d66cc743c874f8da80a3aad1a4160fc1e
GustavoDelaiOnzi <gustavodelai11@gmail.com>
2020-10-24 23:43:14 -0300

    '

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,13 @@ import os
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 DATABASES = {
     'default': {
