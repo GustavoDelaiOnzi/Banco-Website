7a8b305201240bbc2d1f82ce0ad5c0bb31027dd4
GustavoDelaiOnzi <gustavodelai11@gmail.com>
2020-10-25 00:58:48 -0300

    '

--- banco/static/banco/script.js
+++ banco/static/banco/script.js
@@ -2,14 +2,20 @@ function debito(){
     document.getElementById("todas-transacoesid").className = "hidden";
     document.getElementById("transacoes-debitoid").className = "todas-transacoes";
     document.getElementById("transacoes-creditoid").className = "hidden";
+    document.getElementById("mostrarsaldo").className = "hidden";
+    document.getElementById("mostrarsaldo2").className = "hidden";
 }
 function todas(){
     document.getElementById("todas-transacoesid").className = "todas-transacoes";
     document.getElementById("transacoes-debitoid").className = "hidden";
     document.getElementById("transacoes-creditoid").className = "hidden";
+    document.getElementById("mostrarsaldo").className = "saldo";
+    document.getElementById("mostrarsaldo2").className = "saldo-bottom";
 }
 function credito(){
     document.getElementById("todas-transacoesid").className = "hidden";
     document.getElementById("transacoes-debitoid").className = "hidden";
     document.getElementById("transacoes-creditoid").className = "todas-transacoes";
+    document.getElementById("mostrarsaldo").className = "hidden";
+    document.getElementById("mostrarsaldo2").className = "hidden";
 }
